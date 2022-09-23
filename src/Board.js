233aa08d057191a5b7963395202cc2a056a8cbf2
NameIsFill <filpopowski@gmail.com>
2022-09-23 15:01:00 +0200

    working player movement

--- src/Board.js
+++ src/Board.js
@@ -34,12 +34,81 @@ class Board {
   }
 
   movePlayer(keyboardKey) {
+    //up
+    if (keyboardKey.keyCode === 38) {
+      if(this.playerYIndex - 1 >= 0) {
+        this.playerYIndex = this.playerYIndex - 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
+    }
+    //right-up
+    else if (keyboardKey.keyCode === 33) {
+      if(this.playerYIndex - 1 >= 0 && this.playerXIndex + 1 <= 19) {
+        this.playerYIndex = this.playerYIndex - 1;
+        this.playerXIndex = this.playerXIndex + 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
+    }
+    //right
+    else if (keyboardKey.keyCode === 39) {
+      if(this.playerXIndex + 1 <= 19) {
+        this.playerXIndex = this.playerXIndex + 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
+    }
+    //right-down
+    else if (keyboardKey.keyCode === 34) {
+      if(this.playerXIndex + 1 <= 19 && this.playerYIndex + 1 <= 19) {
+        this.playerXIndex = this.playerXIndex + 1;
+        this.playerYIndex = this.playerYIndex + 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
+    }
+    //down
+    else if (keyboardKey.keyCode === 40) {
+      if(this.playerYIndex + 1 <= 19) {
+        this.playerYIndex = this.playerYIndex + 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
+    }
+    //left-down
+    else if (keyboardKey.keyCode === 35) {
+      if(this.playerYIndex + 1 <= 19 && this.playerXIndex - 1 >= 0) {
+        this.playerYIndex = this.playerYIndex + 1;
+        this.playerXIndex = this.playerXIndex - 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
+    }
     //left
-    if (keyboardKey.keyCode === 37) {
-      this.playerXIndex = this.playerXIndex - 1;
-      this.player.divElement.classList.remove('player-tile');
-      this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
-      this.player.divElement.classList.add('player-tile');
+    else if (keyboardKey.keyCode === 37) {
+      if(this.playerXIndex - 1 >= 0) {
+        this.playerXIndex = this.playerXIndex - 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
+    }
+    //left-up
+    else if (keyboardKey.keyCode === 36) {
+      if(this.playerXIndex - 1 >= 0 && this.playerYIndex - 1 >= 0) {
+        this.playerXIndex = this.playerXIndex - 1;
+        this.playerYIndex = this.playerYIndex - 1;
+        this.player.divElement.classList.remove('player-tile');
+        this.player = this.gameGrid[this.playerYIndex][this.playerXIndex];
+        this.player.divElement.classList.add('player-tile');
+      }
     }
   }
 }
